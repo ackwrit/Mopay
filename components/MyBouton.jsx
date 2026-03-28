4638bbd6eb3423ce379d5afa10ed0320da2c635a
skweel <dissingardjino@icloud.com>
2026-03-28 23:14:49 +0100

    mise en place du bloc paiement et sous total pur le design

--- components/MyBouton.jsx
+++ components/MyBouton.jsx
@@ -1,11 +1,17 @@
 import { TouchableOpacity ,Text, View} from "react-native";
 import { styles } from "./MyBouton.style";
+import { FontAwesome } from "@expo/vector-icons";
 
-export function MyBouton({onPress, text}){
+export function MyBouton({onPress, text,icon}){
     return (
         
             <TouchableOpacity style ={styles.container} onPress={onPress}>
-            <Text style={styles.texte}>{text}</Text>
+                <View style={{flexDirection:"row"}}>
+                    <FontAwesome name={icon===null?"":icon}/>
+                    <Text style={styles.texte}>{text}</Text>
+
+                </View>
+            
         </TouchableOpacity>
 
         
--- view/AddInvoices.jsx
+++ view/AddInvoices.jsx
@@ -1,4 +1,4 @@
-import { View ,Text, ImageBackground, TouchableOpacity, ScrollView} from "react-native";
+import { View ,Text, ImageBackground, TouchableOpacity, ScrollView,Modal} from "react-native";
 import { styles } from "../styles/App.style";
 import background from '../assets/background.png'
 import { FontAwesome } from "@expo/vector-icons";
@@ -6,12 +6,15 @@ import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { getClients } from "../services/database";
 import { useCallback, useEffect, useState } from "react";
 import { MyCardClient } from "../components/MyCardClient";
+import { MyTextButton } from "../components/MyTextButton";
+import {MyBouton} from "../components/MyBouton";
 
 export function AddInvoices(){
     const nav = useNavigation();
     const [selectClient,setselectedClient]= useState();
     const [listclient,setlistclient] = useState([]);
     const [open,setOpen] = useState(false);
+    const [modalVisible,setModalVissible] = useState(false);
 
     useFocusEffect(
         useCallback(()=>{
@@ -45,10 +48,23 @@ export function AddInvoices(){
 
     }
 
+    function validation(){
+        setModalVissible(false);
+    }
+
     
 
     return (
         <ImageBackground source={background} imageStyle={{opacity:0.4}} style={{flex:1,padding:10}}>
+            <Modal visible={modalVisible} animationType="slide" >
+                  <ImageBackground source={background} style={{flex:1}} imageStyle={{opacity:0.4}}>
+                       <Text>Ajouter un service</Text>
+                <MyBouton text={"Valider"} onPress={validation}/>
+
+                  </ImageBackground>
+             
+
+            </Modal>
             <TouchableOpacity onPress={back}>
                  <View style={styles.header_Addinvoice}>
                 <FontAwesome name="angle-left" size={30} style={{marginRight:5}}/>
@@ -105,8 +121,87 @@ export function AddInvoices(){
 
             {
                 !open && (
-                    <View>
-                <Text>Bloc article/service</Text>
+                    <View style={{
+                        flex:1,
+                        backgroundColor:"#F7F9FB", 
+                        margin:10,
+                        borderRadius : 20,
+                        borderWidth : 1,
+                        borderColor :"black",
+                        padding : 10
+                        }}>
+                <View style={{alignItems:"center",marginVertical:5}}>
+
+                    <Text style={styles.title}>Article/Service</Text>
+
+                </View>
+                <View style={{height:"40%",padding:10,marginBottom:5}}>
+                    <ScrollView>
+                        <Text>List des services</Text>
+
+                    </ScrollView>
+                    
+                </View>
+                <MyTextButton color={"blue"} texte={"+ ajouter article/service"} onPress={()=>setModalVissible(true)}/>
+                <View style={{
+                    marginTop:5,
+                    backgroundColor:"white",
+                    height:"20%",
+                    justifyContent:"space-around",
+                    padding:10,
+                    borderRadius : 20,
+                    borderWidth : 1,
+                    borderColor :"black"
+
+                    }}>
+                    <View style={{flexDirection:"row",justifyContent:"space-between"}}>
+                        <Text style={{color:"grey"}}>Sous total</Text>
+                        <Text>montant Ar</Text>
+
+                    </View>
+                    <View style={{flexDirection:"row",justifyContent:"space-between"}}>
+                        <Text style={{color:"grey"}}>Frais de service</Text>
+                        <Text>montant Ar</Text>
+
+                    </View>
+                    <View style={{flexDirection:"row",justifyContent:"space-between"}}>
+                        <Text>Total à payer</Text>
+                        <Text>montant Ar</Text>
+
+                    </View>
+                    
+                    
+                    
+                </View>
+                <View style={{width :"100%",alignItems:"center",marginTop:10}}>
+                    <MyBouton text={"Créer la facture"}/>
+
+                </View>
+                <View style={{
+                    width :"100%",
+                    alignItems:"center",
+                    marginTop:5,
+                    backgroundColor:"#3CB54A",
+                    height:35,
+                    justifyContent :"center",
+                    flexDirection :"row",
+                    borderRadius : 20,
+                    borderColor :"black",
+                    borderWidth : 1
+                    }}>
+                        <FontAwesome name="check-circle" size={20} color={"white"}/>
+                        
+
+                    
+                    <Text style={{marginLeft:5,color:"white"}}>Recevez montant Ar sur votre compte mobile</Text>
+                    
+                </View>
+
+                
+
+                
+                
+                
                     </View>
 
                 )
